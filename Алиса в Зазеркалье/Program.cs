77a3306a5b1a55745b60ea34534c003b30eae8dc
Toschyk <74979512+Toschyk@users.noreply.github.com>
2025-03-15 09:56:16 +0700

    Update Program.cs

--- Алиса в Зазеркалье/Program.cs
+++ Алиса в Зазеркалье/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace Алиса_в_Зазеркалье
+namespace Alice
 {
     class Program
     {
@@ -14,8 +10,17 @@ namespace Алиса_в_Зазеркалье
             Console.Write("Введите значение: ");
             string name = Console.ReadLine();
 
+            string reversedName = ReverseString(name);
 
-            char[] vs = name.ToCharArray();
+            Console.Write($"Зеркально: ");
+            Console.WriteLine(reversedName);
+            Console.ReadKey();
+        }
+
+        // Метод, который переворачивает строку
+        public static string ReverseString(string input)
+        {
+            char[] vs = input.ToCharArray();
             char temp;
 
             for (int i = 0; i < vs.Length / 2; i++)
@@ -25,9 +30,7 @@ namespace Алиса_в_Зазеркалье
                 vs[(vs.Length - 1) - i] = temp;
             }
 
-            Console.Write($"Зеркально: ");
-            Console.WriteLine(new string(vs));
-            Console.ReadKey();
+            return new string(vs);
         }
     }
 }
